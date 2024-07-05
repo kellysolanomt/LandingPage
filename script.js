d593dc5e514d32427bd251ba5f5e98f6b9249a41
Kelly Solano <kelly.solano.1403@gmail.com>
2024-07-05 18:48:29 -0500

    Configuración de menu desplegable

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-function menu_burger(){
-    let x = document.querySelector('.home-navbar-burgermenu');
-    
+document.addEventListener('DOMContentLoaded', function() {
+    const burgerMenu = document.querySelector('.home-navbar-burgermenu');
+    const closeMenu = document.querySelector('.home-navbar-closemenu');
+    const menuWindow = document.querySelector('.home-navbar-burgermenu-menu');
 
-}
+    if (burgerMenu && closeMenu && menuWindow) {
+        const show = () => {
+            menuWindow.style.display = 'flex';
+            menuWindow.style.top = '0';
+            console.log('Se abrió');
+        }
+
+        const close = () => {
+            menuWindow.style.top = '-100%';
+            menuWindow.style.display = 'none';
+        }
+
+        burgerMenu.addEventListener('click', show);
+        closeMenu.addEventListener('click', close);
+    } else {
+        console.log('Alguno de los elementos no se encontró');
+    }
+});
